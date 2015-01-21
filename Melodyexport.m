fd6d802c5413290cd8890252633fdeabcefcd5d3
k4733294 <k4733294@gmail.com>
2015-01-21 19:03:06 +0800

    10:34 - 11:10#program fix the error of note length and note exist at note in the measure bug + #Debug MelodyExport log at (ga flowing complete ver1)
    15:06 - 16:06#program fix MidiExport for fixed Getmeasurecontent×2

--- Melodyexport.m
+++ Melodyexport.m
@@ -4,58 +4,26 @@ sizeMM = size(mainMelody.notesInTheMeasure,1);
 N = sizeMM;  % num notes
 M = zeros(N,6);
 ticksPerSixteenthNote = mainMelody.ticksPerQuarterNote/4;
-duration=0;
 countNodePrev = 1;
-preTDT=0;
-countMultiNote=1;
-MultiNote=zeros(1,10);
-
 
 for i = 1: sizeMM
 totalDeltaTimes = mainMelody.notesInTheMeasure(i,1)*ticksPerSixteenthNote;
     if (mainMelody.notesInTheMeasure(i,5) == -1)
-    elseif (mainMelody.notesInTheMeasure(i,5) <= -2)
-        if MultiNote(1,1)==1;
-           duration = duration + ticksPerSixteenthNote;
-           M(countNodePrev-1,5) = totalDeltaTimes - duration - ticksPerSixteenthNote ;  % note on:  notes start every .5 seconds 
-           M(countNodePrev-1,6) = totalDeltaTimes;   % note off: each note has duration .5 seconds  
-        else
-            pRestNote=abs(mainMelody.notesInTheMeasure(i,5));
-            whichRestNote = find(MultiNote==pRestNote,1);
-                if preTDT==totalDeltaTimes
-                    %do nothing
-                else
-                duration = duration + ticksPerSixteenthNote;
-                end
-            M(countNodePrev-(countMultiNote-whichRestNote+1),5) =  totalDeltaTimes - duration - ticksPerSixteenthNote ;  % note on:  notes start every .5 seconds 
-            M(countNodePrev-(countMultiNote-whichRestNote+1),6) = totalDeltaTimes;   % note off: each note has duration .5 seconds  
-        end
-   else
+    elseif (mainMelody.notesInTheMeasure(i,3) ~= 0)
+           noteDeltaTimesLength = mainMelody.notesInTheMeasure(i,3)*ticksPerSixteenthNote;
            M(countNodePrev,1) = 1;         % all in track 1
            M(countNodePrev,2) = 1;         % all in channel 1
            M(countNodePrev,3) = mainMelody.notesInTheMeasure(i,5);
            M(countNodePrev,4) = mainMelody.notesInTheMeasure(i,6);
-           M(countNodePrev,5) = totalDeltaTimes - ticksPerSixteenthNote;  % note on:  notes start every .5 seconds    
-           M(countNodePrev,6) = totalDeltaTimes;   % note off: each note has duration .5 seconds 
+           M(countNodePrev,5) = totalDeltaTimes - ticksPerSixteenthNote ;  % note on:  notes start every .5 seconds    
+           M(countNodePrev,6) = totalDeltaTimes + noteDeltaTimesLength - ticksPerSixteenthNote;   % note off: each note has duration .5 seconds 
            countNodePrev = countNodePrev+1;
-           duration=0;
-           
-            if preTDT == totalDeltaTimes
-                countMultiNote= countMultiNote+1;
-                MultiNote(1,1) = 2;
-                MultiNote(1,countMultiNote) = MultiNote(1,countMultiNote-1) +1;
-            else
-                countMultiNote=1;
-                MultiNote(1,countMultiNote) =1;
-            end
-   end
-preTDT = totalDeltaTimes;
+    else
+    end
 end
 totalRow=find(M(:,1) ,1,'last' );
 resizeM = zeros(totalRow,6);
 resizeM = M(1:totalRow,:);
-
-
 
 %midi_new = matrix2midi(resizeM,mainMelody);
 midi_new = matrix2midi(resizeM,mainMelody.ticksPerQuarterNote);
@@ -89,4 +57,3 @@ ylabel('note number');
    
 end
 %}
-a=1;
